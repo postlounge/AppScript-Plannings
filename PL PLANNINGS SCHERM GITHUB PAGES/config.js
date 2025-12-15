910c158b2dadfaf2038b76fb7bc6fa5af0fbbe13
MM Postlounge <sonny@postlounge.com>
2025-12-15 13:23:38 +0100

    2025 terug gezet

--- PL PLANNINGS SCHERM GITHUB PAGES/config.js
+++ PL PLANNINGS SCHERM GITHUB PAGES/config.js
@@ -13,8 +13,8 @@
  * 
  * @type {string}
  */
-/*const APPSCRIPT_API_URL = 'https://script.google.com/macros/s/AKfycbyJn8ARlhM8ROSVL3KvXs9pXr9h-s8N7xSZzj2sB0j_b7qlBUwFux8U4MzePiQ0XH8Epg/exec';*/ /* 2025 versie*/
-const APPSCRIPT_API_URL = 'https://script.google.com/macros/s/AKfycby9ghacL7eptkBWzoTGz8f2a4dnGgcl2IkkEJJV70QwJJhjv5Q7UyoWmOEC_Mbi7vR78A/exec'; /* nieuwe opzet planning versie*/
+const APPSCRIPT_API_URL = 'https://script.google.com/macros/s/AKfycbyJn8ARlhM8ROSVL3KvXs9pXr9h-s8N7xSZzj2sB0j_b7qlBUwFux8U4MzePiQ0XH8Epg/exec'; /* 2025 versie */
+/*const APPSCRIPT_API_URL = 'https://script.google.com/macros/s/AKfycby9ghacL7eptkBWzoTGz8f2a4dnGgcl2IkkEJJV70QwJJhjv5Q7UyoWmOEC_Mbi7vR78A/exec'; nieuwe opzet planning versie */
 
 /**
  * SECTION_COLUMNS configuratie
